1659fa2d02fa1816f67efd8361b20c06ece1cf7c
benrx <benrx@benxiao-surf01.lan>
2020-06-17 12:43:21 -0700

    Use BGR2RGB for mat2Image for more efficiency and less memory bloat

--- src/main/java/com/rpicam/video/VideoUtils.java
+++ src/main/java/com/rpicam/video/VideoUtils.java
@@ -1,30 +1,25 @@
 package com.rpicam.video;
 
-import java.io.ByteArrayInputStream;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.WritableImage;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 
 public class VideoUtils {
     public static Image toJFXImage(Mat mat) {
-        var bgraMat = new Mat();
-        Imgproc.cvtColor(mat, bgraMat, Imgproc.COLOR_BGR2BGRA);
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB);
         
-        var w = bgraMat.cols();
-        var h = bgraMat.rows();
-        var channels = 4;
+        var w = mat.cols();
+        var h = mat.rows();
+        var channels = 3;
         
         var imageArray = new byte[w * h * channels];
-        bgraMat.get(0, 0, imageArray);
+        mat.get(0, 0, imageArray);
         var img = new WritableImage(w, h);
         var pw = img.getPixelWriter();
-        pw.setPixels(0, 0, w, h, PixelFormat.getByteBgraPreInstance(), imageArray, 0, w * channels);
+        pw.setPixels(0, 0, w, h, PixelFormat.getByteRgbInstance(), imageArray, 0, w * channels);
         return img;
     }
 }
