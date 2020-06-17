5233539ca57ababc9826ce10c13aeb9df2ebb89e
Benjamin Xiao <ben.r.xiao@gmail.com>
2020-06-17 11:33:33 -0700

    Add experimental mat to jfx conversion function

--- src/main/java/com/rpicam/video/VideoUtils.java
+++ src/main/java/com/rpicam/video/VideoUtils.java
@@ -2,6 +2,8 @@ package com.rpicam.video;
 
 import java.io.ByteArrayInputStream;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.WritableImage;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -14,6 +16,17 @@ public class VideoUtils {
         Imgcodecs.imencode("*.bmp", mat, bytes);
         ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes.toArray());
         Image img = new Image(inputStream);
+        return img;
+    }
+    
+    public static Image toJFXImageFast(Mat mat) {
+        var w = mat.cols();
+        var h = mat.rows();
+        var imageArray = new byte[w * h * 3];
+        mat.get(0, 0, imageArray);
+        var img = new WritableImage(w, h);
+        var pw = img.getPixelWriter();
+        pw.setPixels(0, 0, w, h, PixelFormat.getByteRgbInstance(), imageArray, 0, w * 3);
         return img;
     }
 }
