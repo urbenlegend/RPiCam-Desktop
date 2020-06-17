56e55206df5db0f8bfc19817c7c3506329d8b903
Benjamin Xiao <ben.r.xiao@gmail.com>
2020-06-17 12:29:55 -0700

    Switch toJFXImage function to use faster conversion method

--- src/main/java/com/rpicam/video/VideoUtils.java
+++ src/main/java/com/rpicam/video/VideoUtils.java
@@ -4,29 +4,27 @@ import java.io.ByteArrayInputStream;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.WritableImage;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 
 public class VideoUtils {
     public static Image toJFXImage(Mat mat) {
-        // TODO: Figure out a way to convert without encoding
-        MatOfByte bytes = new MatOfByte();
-        Imgcodecs.imencode("*.bmp", mat, bytes);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes.toArray());
-        Image img = new Image(inputStream);
-        return img;
-    }
-    
-    public static Image toJFXImageFast(Mat mat) {
-        var w = mat.cols();
-        var h = mat.rows();
-        var imageArray = new byte[w * h * 3];
-        mat.get(0, 0, imageArray);
+        var bgraMat = new Mat();
+        Imgproc.cvtColor(mat, bgraMat, Imgproc.COLOR_BGR2BGRA);
+        
+        var w = bgraMat.cols();
+        var h = bgraMat.rows();
+        var channels = 4;
+        
+        var imageArray = new byte[w * h * channels];
+        bgraMat.get(0, 0, imageArray);
         var img = new WritableImage(w, h);
         var pw = img.getPixelWriter();
-        pw.setPixels(0, 0, w, h, PixelFormat.getByteRgbInstance(), imageArray, 0, w * 3);
+        pw.setPixels(0, 0, w, h, PixelFormat.getByteBgraPreInstance(), imageArray, 0, w * channels);
         return img;
     }
 }
