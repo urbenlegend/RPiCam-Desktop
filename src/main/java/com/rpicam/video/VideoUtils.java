0457c44b9ffc7154857876ddd6008c11a3d19837
Benjamin Xiao <ben.r.xiao@gmail.com>
2020-06-22 19:37:08 -0700

    Reduced buffer copies in toJFXImage further. Now there's only one CPU-based buffer copy.

--- src/main/java/com/rpicam/video/VideoUtils.java
+++ src/main/java/com/rpicam/video/VideoUtils.java
@@ -1,10 +1,12 @@
 package com.rpicam.video;
 
+import java.nio.ByteBuffer;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelBuffer;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.WritableImage;
 import static org.bytedeco.opencv.global.opencv_core.ACCESS_READ;
-import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2RGB;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2BGRA;
 import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.UMat;
@@ -12,20 +14,19 @@ import org.bytedeco.opencv.opencv_core.UMat;
 
 public class VideoUtils {
     public static Image toJFXImage(UMat mat) {
-        cvtColor(mat, mat, COLOR_BGR2RGB);
+        UMat bgraMat = new UMat();
+        cvtColor(mat, bgraMat, COLOR_BGR2BGRA);
         
-        var w = mat.cols();
-        var h = mat.rows();
-        var channels = 3;
+        var w = bgraMat.cols();
+        var h = bgraMat.rows();
+        var channels = 4;
         
         var imageArray = new byte[w * h * channels];
-        try (Mat tempMat = mat.getMat(ACCESS_READ)) {
+        try (Mat tempMat = bgraMat.getMat(ACCESS_READ)) {
             tempMat.data().get(imageArray);
         }
-        var img = new WritableImage(w, h);
-        var pw = img.getPixelWriter();
-        pw.setPixels(0, 0, w, h, PixelFormat.getByteRgbInstance(), imageArray, 0, w * channels);
+        var buffer = new PixelBuffer<ByteBuffer>(w, h, ByteBuffer.wrap(imageArray), PixelFormat.getByteBgraPreInstance());
         
-        return img;
+        return new WritableImage(buffer);
     }
 }
